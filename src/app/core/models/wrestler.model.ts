95ad2a71750969e5c52860bd614b947b45595cab
Gile <mladen.reljic87@gmail.com>
2023-03-02 18:01:45 +0100

    Create the wrestler interface

---
+++ src/app/core/models/wrestler.model.ts
@@ -0,0 +1,11 @@
+export interface Wrestler {
+  id: number;
+  name: string;
+  nickname: string;
+  image: string;
+  height: number;
+  weight: number;
+  from: string;
+  debut: Date;
+  specialMoves: string[];
+}
