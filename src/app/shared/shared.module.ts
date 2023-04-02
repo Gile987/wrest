3db3ad5f5f14f3ee9129a3ac7a6a1006f48ee166
Gile <mladen.reljic87@gmail.com>
2023-04-02 16:17:00 +0200

    Add minor styling, delete bubble-header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedRosterModule } from './modules/shared-roster.module';
-import { BubbleHeaderComponent } from './components/bubble-header/bubble-header.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    BubbleHeaderComponent
   ],
   imports: [CommonModule, SharedRosterModule, RouterModule],
-  exports: [SharedRosterModule, BubbleHeaderComponent],
+  exports: [SharedRosterModule],
 })
 export class SharedModule {}
