e2ee33a009eb5529a57c3e71e20c82eb82d61a0a
gile987 <mladen.reljic87@gmail.com>
2023-08-20 15:32:13 +0200

    Refactor the paginator component

--- src/app/shared/components/paginator/paginator.component.ts
+++ src/app/shared/components/paginator/paginator.component.ts
@@ -3,32 +3,34 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
-  styleUrls: ['./paginator.component.scss']
+  styleUrls: ['./paginator.component.scss'],
 })
 export class PaginatorComponent implements OnInit {
-  @Input() currentPage!: number;
-  @Input() pageSize!: number;
-  @Input() totalItems!: number;
-  @Output() pageChange: EventEmitter<number> = new EventEmitter<number>();
+  @Input() public currentPage!: number;
+  @Input() public pageSize!: number;
+  @Input() public totalItems!: number;
+  @Output() public pageChange: EventEmitter<number> =
+    new EventEmitter<number>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  getPages(): number[] {
+  public getPages(): number[] {
     const totalPages: number = this.getTotalPages();
-    return Array(totalPages).fill(0).map((_, index: number): number => index + 1);
+    return Array(totalPages)
+      .fill(0)
+      .map((_, index: number): number => index + 1);
   }
 
-  goToPage(event: MouseEvent, page: number): void {
+  public goToPage(event: MouseEvent, page: number): void {
     event.preventDefault();
     if (page >= 1 && page <= this.getTotalPages()) {
       this.pageChange.emit(page);
     }
   }
 
-  getTotalPages(): number {
+  public getTotalPages(): number {
     return Math.ceil(this.totalItems / this.pageSize);
   }
 }
