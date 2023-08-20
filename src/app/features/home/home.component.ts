94b0564fa3643933679e52e174d45ceeedc3c121
gile987 <mladen.reljic87@gmail.com>
2023-08-20 15:28:06 +0200

    Create a fetchCurrentUser method in home component and call it in ngOnInit

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -6,15 +6,18 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  user$: Observable<User | null> = null!; 
+  user$: Observable<User | null> = new Observable<User | null>();
 
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor(private authenticationService: AuthenticationService) {}
 
   ngOnInit(): void {
-    this.user$ = this.authenticationService.getCurrentUser(); 
+    this.fetchCurrentUser();
   }
 
+  private fetchCurrentUser(): void {
+    this.user$ = this.authenticationService.getCurrentUser();
+  }
 }
