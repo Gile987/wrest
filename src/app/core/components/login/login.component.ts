b11bee14ed78cd001580540a7d74e636d6c5dbec
gile987 <mladen.reljic87@gmail.com>
2023-08-20 14:56:56 +0200

    Utilize rxjs fully in the login component

--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { User } from '../../models/user.model';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 import { AuthenticationService } from '../../services/authentication.service';
-import { Subscription } from 'rxjs';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  loginForm: FormGroup;
-  users: User[] = [];
-  isAuthenticated: Boolean = false;
-  private authSubscription: Subscription | undefined;
+  public loginForm: FormGroup;
+  public users: User[] = [];
+  public isAuthenticated: boolean = false;
+  private unsubscribe$: Subject<void> = new Subject<void>();
 
   constructor(
     private formBuilder: FormBuilder,
@@ -29,36 +31,48 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.loadUsers();
-    this.authSubscription = this.authenticationService.getCurrentUser().subscribe((user) => {
-      this.isAuthenticated = Boolean(user);
-    });
+    this.getCurrentUser();
   }
 
   ngOnDestroy(): void {
-    if (this.authSubscription) {
-      this.authSubscription.unsubscribe();
-    }
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   private loadUsers(): void {
-    this.authenticationService.getUsers().subscribe((users: User[]) => {
-      this.users = users;
-    });
+    this.authenticationService
+      .getUsers()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((users: User[]) => {
+        this.users = users;
+      });
   }
 
-  login(): void {
+  public login(): void {
     if (this.loginForm.valid) {
       const email: string = this.loginForm.get('email')?.value;
       const password: string = this.loginForm.get('password')?.value;
 
-      this.authenticationService.authenticateUser(email, password).subscribe((user: User | null) => {
-        if (user) {
-          console.log('User logged in');
-          this.router.navigate(['']);
-        } else {
-          console.log('User not found or wrong password');
-        }
-      });
+      this.authenticationService
+        .authenticateUser(email, password)
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe((user: User | null) => {
+          if (user) {
+            console.log('User logged in');
+            this.router.navigate(['']);
+          } else {
+            console.log('User not found or wrong password');
+          }
+        });
     }
+  }
+
+  private getCurrentUser(): void {
+    this.authenticationService
+      .getCurrentUser()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((user: User | null) => {
+        this.isAuthenticated = Boolean(user);
+      });
   }
 }
