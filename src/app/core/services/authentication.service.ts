65375c385a7762f957ffdb8b7b01e238cdd721f7
gile987 <mladen.reljic87@gmail.com>
2023-08-20 15:21:58 +0200

    Refactor the authentication service - edit deprecated code. Refactor the roster service

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -4,34 +4,37 @@ import { Observable, of, tap, BehaviorSubject } from 'rxjs';
 import { User } from '../models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
   private usersUrl: string = '/assets/users.json';
   private users: User[] = [];
 
-  private currentUserSubject: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
+  private currentUserSubject: BehaviorSubject<User | null> =
+    new BehaviorSubject<User | null>(null);
 
   constructor(private http: HttpClient) {
     const storedUser = localStorage.getItem('currentUser');
-    this.currentUserSubject = new BehaviorSubject<User | null>(storedUser ? JSON.parse(storedUser) : null);
+    this.currentUserSubject = new BehaviorSubject<User | null>(
+      storedUser ? JSON.parse(storedUser) : null
+    );
   }
 
-  getUsers(): Observable<User[]> {
+  public getUsers(): Observable<User[]> {
     if (this.users.length > 0) {
       return of(this.users);
     } else {
-      return this.http.get<User[]>(this.usersUrl).pipe(
-        tap((users: User[]) => this.users = users)
-      );
+      return this.http
+        .get<User[]>(this.usersUrl)
+        .pipe(tap((users: User[]) => (this.users = users)));
     }
   }
 
-  getCurrentUser(): Observable<User | null> {
+  public getCurrentUser(): Observable<User | null> {
     return this.currentUserSubject.asObservable();
   }
 
-  setCurrentUser(user: User | null): void {
+  public setCurrentUser(user: User | null): void {
     this.currentUserSubject.next(user);
     if (user) {
       localStorage.setItem('currentUser', JSON.stringify(user));
@@ -40,17 +43,22 @@ export class AuthenticationService {
     }
   }
 
-  authenticateUser(email: string, password: string): Observable<User | null> {
-    const user: User | undefined = this.users.find((u: User) => u.email === email && u.password === password);
+  public authenticateUser(
+    email: string,
+    password: string
+  ): Observable<User | null> {
+    const user: User | undefined = this.users.find(
+      (u: User) => u.email === email && u.password === password
+    );
     this.setCurrentUser(user || null);
     return of(user || null);
   }
 
-  registerUser(user: User): Observable<User | null> {
+  public registerUser(user: User): Observable<User | null> {
     return this.http.post<User>(this.usersUrl, user);
   }
 
-  isAuthenticated(): boolean {
+  public isAuthenticated(): boolean {
     return localStorage.getItem('currentUser') !== null;
   }
 }
--- src/app/core/services/tjpw-roster.service.ts
+++ src/app/core/services/tjpw-roster.service.ts
@@ -1,38 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { BehaviorSubject, Observable, catchError, map } from 'rxjs';
 import { Wrestler } from '../models/wrestler.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TjpwRosterService {
-  private wrestlersSubject: BehaviorSubject<Wrestler[]> = new BehaviorSubject<Wrestler[]>([]);
-  readonly wrestlers$: Observable<Wrestler[]> = this.wrestlersSubject.asObservable();
+  private wrestlersSubject: BehaviorSubject<Wrestler[]> = new BehaviorSubject<
+    Wrestler[]
+  >([]);
+  readonly wrestlers$: Observable<Wrestler[]> =
+    this.wrestlersSubject.asObservable();
 
   constructor(private http: HttpClient) {
     this.loadWrestlers();
   }
 
   private loadWrestlers(): void {
-    this.http.get<{wrestlers: Wrestler[]}>('./assets/wrestlers.json').pipe(
-      catchError((err: unknown): Observable<never> => {
-        console.error('Error loading wrestlers', err);
-        return throwError('Unable to load wrestlers');
-      }),
-      map((data: {wrestlers: Wrestler[]}) => data.wrestlers.map(wrestlerData => ({
-        ...wrestlerData,
-        debut: new Date(wrestlerData.debut)
-      }))),
-    ).subscribe((wrestlers: Wrestler[]) => {
-      this.wrestlersSubject.next(wrestlers);
-    });
+    this.http
+      .get<{ wrestlers: Wrestler[] }>('./assets/wrestlers.json')
+      .pipe(
+        catchError((err: any): Observable<never> => {
+          console.error('Error loading wrestlers', err);
+          throw new Error('Unable to load wrestlers');
+        }),
+        map((data: { wrestlers: Wrestler[] }) =>
+          data.wrestlers.map((wrestlerData: Wrestler) => ({
+            ...wrestlerData,
+            debut: new Date(wrestlerData.debut),
+          }))
+        )
+      )
+      .subscribe((wrestlers: Wrestler[]) => {
+        this.wrestlersSubject.next(wrestlers);
+      });
   }
 
-  getWrestlerById(id: number): Observable<Wrestler | undefined> {
+  public getWrestlerById(id: number): Observable<Wrestler | undefined> {
     return this.wrestlers$.pipe(
-      map((wrestlers: Wrestler[]) => wrestlers.find(wrestler => wrestler.id === id))
+      map((wrestlers: Wrestler[]) =>
+        wrestlers.find((wrestler) => wrestler.id === id)
+      )
     );
   }
 }
