73ffaab68f8417514b1ee39ac56f1bd770822aa2
gile987 <mladen.reljic87@gmail.com>
2023-08-22 20:59:05 +0200

    Add checking that disables selecting second wrestler first, and remove already selected wrestler from the second list

--- src/app/features/play/play.component.ts
+++ src/app/features/play/play.component.ts
@@ -10,11 +10,16 @@ import { Wrestler } from '../../core/models/wrestler.model';
 export class PlayComponent implements OnInit {
   public wrestlers: Wrestler[] = [];
   public availableWrestlersForB: Wrestler[] = [];
-  public selectedWrestlers: Wrestler[] = [];
+  public selectedWrestlers: (Wrestler | null)[] = [null, null];
+  public wrestlerSelectedInColumnA: boolean = false;
 
   constructor(private rosterService: TjpwRosterService) {}
 
   ngOnInit(): void {
+    this.subscribeToWrestlers();
+  }
+
+  private subscribeToWrestlers(): void {
     this.rosterService.wrestlers$.subscribe((wrestlers: Wrestler[]) => {
       this.wrestlers = wrestlers;
       this.availableWrestlersForB = [...wrestlers];
@@ -22,20 +27,25 @@ export class PlayComponent implements OnInit {
   }
 
   public selectWrestler(wrestlerId: number, wrestlerIndex: number): void {
-    const selectedWrestler: Wrestler | undefined = this.wrestlers.find(
+    const selectedWrestler = this.wrestlers.find(
       (wrestler: Wrestler) => wrestler.id === wrestlerId
     );
 
-    if (
-      selectedWrestler &&
-      this.selectedWrestlers.length < 2 &&
-      !this.selectedWrestlers.includes(selectedWrestler)
-    ) {
-      this.selectedWrestlers[wrestlerIndex] = selectedWrestler;
-      if (wrestlerIndex === 0) {
-        this.availableWrestlersForB = this.availableWrestlersForB.filter(
-          (wrestler: Wrestler) => wrestler.id !== wrestlerId
-        );
+    if (wrestlerIndex === 0) {
+      this.wrestlerSelectedInColumnA = true;
+      this.selectedWrestlers[wrestlerIndex] = selectedWrestler || null;
+      this.availableWrestlersForB = this.wrestlers.filter(
+        (wrestler: Wrestler) => wrestler.id !== wrestlerId
+      );
+
+      this.selectedWrestlers[1] = null;
+    } else if (wrestlerIndex === 1 && this.wrestlerSelectedInColumnA) {
+      if (
+        selectedWrestler &&
+        this.selectedWrestlers.length < 2 &&
+        !this.selectedWrestlers.includes(selectedWrestler)
+      ) {
+        this.selectedWrestlers[wrestlerIndex] = selectedWrestler;
       }
     }
   }
