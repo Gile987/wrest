99155d19316b6eab5396fa76001b096987a0c129
gile987 <mladen.reljic87@gmail.com>
2023-08-24 17:05:07 +0200

    Create starting logic for match simulation

--- src/app/features/play/play.component.ts
+++ src/app/features/play/play.component.ts
@@ -71,7 +71,46 @@ export class PlayComponent implements OnInit {
 
   public simulateMatch(): void {
     if (this.canSimulateMatch()) {
-      console.log('Simulating match between:');
+      const wrestlerA = this.selectedWrestlers[0];
+      const wrestlerB = this.selectedWrestlers[1];
+
+      if (wrestlerA && wrestlerB) {
+        const minTurns = 10;
+        const maxTurns = 20;
+        const numTurns =
+          Math.floor(Math.random() * (maxTurns - minTurns + 1)) + minTurns;
+
+        const wrestlingMoves: any[] = [
+          { name: 'Punch', damage: 10, accuracy: 0.8 },
+          { name: 'Kick', damage: 15, accuracy: 0.7 },
+        ];
+
+        const simulateTurn = (wrestler: Wrestler, opponent: Wrestler) => {
+          const move =
+            wrestlingMoves[Math.floor(Math.random() * wrestlingMoves.length)];
+          const shouldPerformAdditionalMove = Math.random() < 0.3;
+          if (shouldPerformAdditionalMove) {
+            simulateTurn(wrestler, opponent);
+          }
+          console.log(
+            `${wrestler.name} performs ${move.name} on ${opponent.name}`
+          );
+        };
+
+        for (let i = 0; i < numTurns; i++) {
+          simulateTurn(wrestlerA, wrestlerB);
+          simulateTurn(wrestlerB, wrestlerA);
+        }
+
+        const winner = Math.random() < 0.5 ? wrestlerA : wrestlerB;
+        const specialMove =
+          wrestlerA.specialMoves[
+            Math.floor(Math.random() * wrestlerA.specialMoves.length)
+          ];
+        console.log(
+          `${winner.name} performs the special move ${specialMove} and wins the match!`
+        );
+      }
     }
   }
 }
