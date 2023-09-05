503d0ec7faf0a613938c54100a3f53ac3e4c75be
gile987 <mladen.reljic87@gmail.com>
2023-09-05 15:27:34 +0200

    Create the component that allows the user to edit the rules

---
+++ src/app/features/play/match-settings/match-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-match-settings',
+  templateUrl: './match-settings.component.html',
+  styleUrls: ['./match-settings.component.scss']
+})
+export class MatchSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/features/play/play.module.ts
+++ src/app/features/play/play.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { PlayComponent } from './play.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MatchSettingsComponent } from './match-settings/match-settings.component';
 
 const routes: Routes = [{ path: '', component: PlayComponent }];
 
 @NgModule({
-  declarations: [PlayComponent],
+  declarations: [PlayComponent, MatchSettingsComponent],
   imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
 })
 export class PlayModule {}
