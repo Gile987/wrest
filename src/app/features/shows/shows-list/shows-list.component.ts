252f3c920e0f3db64aa7d51b5859629de8791dcf
gile987 <mladen.reljic87@gmail.com>
2023-09-09 12:54:30 +0200

    Clean up the subscriptions when shows list component is destroyed

--- src/app/features/shows/shows-list/shows-list.component.ts
+++ src/app/features/shows/shows-list/shows-list.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shows-list',
   templateUrl: './shows-list.component.html',
   styleUrls: ['./shows-list.component.scss'],
 })
-export class ShowsListComponent implements OnInit {
+export class ShowsListComponent implements OnInit, OnDestroy {
   shows: any[] = [];
+  private componentDestroyed$: Subject<void> = new Subject<void>();
 
   constructor(private http: HttpClient) {}
 
@@ -15,9 +18,17 @@ export class ShowsListComponent implements OnInit {
     this.fetchShows();
   }
 
+  ngOnDestroy(): void {
+    this.componentDestroyed$.next();
+    this.componentDestroyed$.complete();
+  }
+
   fetchShows(): void {
-    this.http.get<any[]>('./assets/shows.json').subscribe((data) => {
-      this.shows = data;
-    });
+    this.http
+      .get<any[]>('./assets/shows.json')
+      .pipe(takeUntil(this.componentDestroyed$))
+      .subscribe((data) => {
+        this.shows = data;
+      });
   }
 }
